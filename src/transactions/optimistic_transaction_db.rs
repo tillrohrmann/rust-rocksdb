dd41813ed7dbec64e9f12e4113665480d377c9a6
Ahmed Farghal <me@asoli.dev>
2024-06-20 11:07:39 +0100

    Use smartstring as BTreeMap key

--- src/transactions/optimistic_transaction_db.rs
+++ src/transactions/optimistic_transaction_db.rs
@@ -186,7 +186,7 @@ impl<T: ThreadMode> OptimisticTransactionDB<T> {
             }
 
             for (cf_desc, inner) in cfs_v.iter().zip(cfhandles) {
-                cf_map.insert(cf_desc.name.clone(), inner);
+                cf_map.insert(cf_desc.name.clone().into(), inner);
             }
         }
 
